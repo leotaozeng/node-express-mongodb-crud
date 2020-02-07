ed03e7914bd1a996a82942c188a772f5e222f8c3
Zeng Tao <qingchun1995@gmail.com>
2020-02-07 21:01:38 +0800

    First commit

---
+++ models/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://Leo:db19950723@cluster0-koiw6.mongodb.net/employeeDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (error) => {
+  if (!error) {
+    console.log('MongoDB Connection Succeeded!')
+  } else {
+     console.log(`Error in MongoDB Connection: ${error}`)
+  }
+})
