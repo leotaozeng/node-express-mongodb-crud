7619243667a141b7a1d156df3b9f124d78a1113b
Zeng Tao <qingchun1995@gmail.com>
2020-02-08 14:57:38 +0800

    Create the employees route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
+const employeesRouter = require('./routes/employees')
 const database = require('./models/database')
 
 const app = express()
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+app.use('/employees', employeesRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
