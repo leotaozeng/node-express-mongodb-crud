15307adcbbc0f3ef21b2d0d49f5f4c25d7f954f8
Zeng Tao <qingchun1995@gmail.com>
2020-02-09 20:21:20 +0800

    Remove the browser horizontal scrollbar

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const logger = require('morgan')
 
 require('./models/database')
 const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
 const adminRouter = require('./routes/admin')
 
 const app = express()
@@ -23,7 +22,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 app.use('/admin', adminRouter)
 
 // catch 404 and forward to error handler
